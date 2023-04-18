668a6b1eaa152bc7d0c3cf19d83c064f3e17377b
Naiem Foysal <naiemfoysal@Naiems-MacBook-Pro.local>
2023-04-18 23:05:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Web3 from 'web3';
+import "./App.css"
 
-function App() {
+const App = () => {
+  let account = null;
+  let accessToken = null;
+  const connect = async () => {
+    if(window.ethereum){
+      await window.ethereum.send('eth_requestAccounts')
+      window.w3 = new Web3(window.ethereum)
+      var accounts = await window.w3.eth.getAccounts()
+      account = accounts[0]
+
+      accessToken = await authenticate()
+      // 
+      console.log("accessToken:>>>>>",accessToken)
+      alert(accessToken)
+
+      let opts = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`
+        }
+      }
+
+      let res = await fetch(`http://localhost:5000/api/v1/wallet/auth/secret`, opts)
+      console.log("Res from secret route:>>", res)
+      alert(await res.text())
+    }
+  }
+
+
+  const authenticate = async() => {
+    // let res = await fetch(`http://localhost:8000/api/v1/nonce?address=${account}`)
+    let res = await fetch(`http://localhost:8000/api/v1/wallet/auth/nonce/${account}`)
+    let resBody = await res.json()
+
+    // console.log(resBody)
+    let signature = await window.w3.eth.personal.sign(resBody.message, account)
+
+    let opts = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${resBody.tempToken}`
+        }
+    }
+
+    res = await fetch(`http://localhost:8000/api/v1/wallet/auth/verify/${signature}`, opts)
+    resBody = await res.json()
+    console.log("resBody:>>>>", resBody)
+
+    return resBody.token
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1>Login With Wallet</h1>
+      <button onClick={connect}>Login</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
